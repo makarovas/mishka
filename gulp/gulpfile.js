304305c6f3bff0c3ce0bd5eccbecfa5acc0e8852
Alexander Makarof <makarofalex@gmail.com>
2018-06-25 17:01:48 +0300

    new gulp config

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -1,14 +1,25 @@
-let gulp = require('gulp');
-let sass = require('gulp-sass');
-let nano = require('gulp-cssnano');
-let autoprefixer = require('gulp-autoprefixer');
-let concat = require('gulp-concat');
-let uglify = require('gulp-uglify');
+'use strict';
+
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const plumber = require('gulp-plumber');
+const nano = require('gulp-cssnano');
+const autoprefixer = require('gulp-autoprefixer');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+const notify = require('gulp-notify');
+const server = require('browser-sync').create();
 
 
 gulp.task('sass', function () {
   return gulp.src(['assets/sass/app.scss'])
     .pipe(sass())
+    .on('error', notify.onError(function (err) {
+      return {
+        title: 'Styles',
+        message: err.message
+      };
+    }))
     .pipe(autoprefixer())
     .pipe(nano())
     .pipe(gulp.dest('./public/css'));
@@ -24,9 +35,10 @@ gulp.task('scripts', function () {
     .pipe(uglify())
     .pipe(gulp.dest('./public/js'));
 });
-
+// Styles
 gulp.task('watch-build', function () {
   gulp.watch('assets/sass/**/**/*.scss', ['sass']);
+  gulp.watch('../*.html').on('change', server.reload)
   gulp.watch('assets/scripts/**/**/*.js', ['scripts']);
   
 });
